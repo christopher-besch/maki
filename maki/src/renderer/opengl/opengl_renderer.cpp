ed54342d7c346fa46fb7e5024c696429ad82b041
christopher-besch <christopher.besch@gmx.de>
2021-12-24 19:32:46 +0100

    correct log level for opengl messages

--- maki/src/renderer/opengl/opengl_renderer.cpp
+++ maki/src/renderer/opengl/opengl_renderer.cpp
@@ -7,6 +7,22 @@
 
 namespace Maki {
 
+inline void log_gl_extra(const std::string& source, const std::string& type, GLuint id, const std::string& severity, const char* message)
+{
+    MAKI_LOG_EXTRA("OpenGL Message: Source: {0} Type: {1} ID: {2} Severity: {3}\n{4}",
+                   source, type, id, severity, message);
+}
+inline void log_gl_warn(const std::string& source, const std::string& type, GLuint id, const std::string& severity, const char* message)
+{
+    MAKI_LOG_WARN("OpenGL MessageMessage from OpenGL: Source: {0} Type: {1} ID: {2} Severity: {3}\n{4}",
+                  source, type, id, severity, message);
+}
+inline void log_gl_error(const std::string& source, const std::string& type, GLuint id, const std::string& severity, const char* message)
+{
+    MAKI_LOG_ERROR("OpenGL MessageMessage from OpenGL: Source: {0} Type: {1} ID: {2} Severity: {3}\n{4}",
+                   source, type, id, severity, message);
+}
+
 OpenGLRenderer::OpenGLRenderer(const std::string& title, uint32_t width, uint32_t height)
     : Renderer(title, width, height)
 {
@@ -17,13 +33,28 @@ OpenGLRenderer::OpenGLRenderer(const std::string& title, uint32_t width, uint32_
         MAKI_RAISE_CRITICAL("Failed to initialize GLEW.");
 
 #ifndef NDEBUG
-    glDebugMessageCallback([](GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei, const GLchar* message, const void*) {
-        MAKI_LOG_ERROR("Message from OpenGL: Source: {0} Type: {1} ID: {2} Severity: {3}\n{4}",
-                       Stringifier::gl_msg_source(source),
-                       Stringifier::gl_msg_type(type),
-                       id,
-                       Stringifier::gl_msg_severity(severity),
-                       message);
+    glDebugMessageCallback([](GLenum source, GLenum type, uint32_t id, GLenum severity, GLsizei, const char* message, const void*) {
+        std::string source_str   = Stringifier::gl_msg_source(source);
+        std::string type_str     = Stringifier::gl_msg_type(type);
+        std::string severity_str = Stringifier::gl_msg_severity(severity);
+
+        switch(severity) {
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            log_gl_extra(source_str, type_str, id, severity_str, message);
+            break;
+        case GL_DEBUG_SEVERITY_LOW:
+            log_gl_extra(source_str, type_str, id, severity_str, message);
+            break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            log_gl_warn(source_str, type_str, id, severity_str, message);
+            break;
+        case GL_DEBUG_SEVERITY_HIGH:
+            log_gl_error(source_str, type_str, id, severity_str, message);
+            break;
+        default:
+            log_gl_error(source_str, type_str, id, severity_str, message);
+            break;
+        }
     },
                            nullptr);
 #endif
